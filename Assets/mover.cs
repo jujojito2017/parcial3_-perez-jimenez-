e4bf7223619f153beba6affc691fa924007f113a
jujojito2017 <32918865+jujojito2017@users.noreply.github.com>
2017-10-19 19:59:17 -0500

    Add files via upload

---
+++ Assets/mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+    public Transform target1;
+    public Transform target2;
+    public float rapidez;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float velocidad = rapidez * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, target1.position, velocidad);
+
+    }
+}
---
+++ Assets/saltar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saltar : MonoBehaviour {
+
+    public Transform target1;
+    public Transform target2;
+    public Transform ball;
+    public int fuerza;
+    public bool salto;
+    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+     if((ball== target1 || ball == target1)&&salto)
+        {
+            GetComponent<Rigidbody>().AddForce(0, fuerza, 0);
+            salto = false;
+        }
+    }
+}
